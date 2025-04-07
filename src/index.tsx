1c65eb69681b891d6c11f142fcdc90b48edd3b20
bellancafr <bellancaf@gmail.com>
2025-04-07 23:08:27 +0100

    small test

--- src/index.tsx
+++ src/index.tsx
@@ -7,6 +7,7 @@ import { SpeedInsights } from "@vercel/speed-insights/react";
 const root = ReactDOM.createRoot(
   document.getElementById('root') as HTMLElement
 );
+//how is itttt?
 
 root.render(
   <React.StrictMode>
